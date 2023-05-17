e3a105ac15ee97cb41eeaecadcf90869b56df0dd
Dmitriy Chistyakov <dim1.chistyakov@gmail.com>
2023-05-17 23:16:33 +0300

    222 360 357

--- src/main/java/org/example/coderun/easy/ChastyElement_360.java
+++ src/main/java/org/example/coderun/easy/ChastyElement_360.java
@@ -4,7 +4,7 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.*;
 
-public class ChastyElement360 {
+public class ChastyElement_360 {
     public static void main(String[] args) throws Exception {
 
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
